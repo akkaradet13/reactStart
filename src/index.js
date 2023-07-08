a261bf6dd68fc9b0dd46cb45b93498ef20a88c85
akkaradet13 <akkaradet8.11.41@gmail.com>
2023-07-08 14:08:36 +0700

    create componet

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// extarnal component
+import HelloComponent from './component/HelloComponent';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// funcrion componet
+// function HelloComponent() {
+//   return <h1>hello</h1>
+// }
+
+// class component
+// class HelloComponent extends React.Component{
+//   render(){
+//     return <h1>react</h1>
+//   }
+// }
+ReactDOM.render(<HelloComponent/>,document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
